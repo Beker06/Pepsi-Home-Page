7a7654935f1668bc3252f3e3bf790177f59a3651
Eliam Vazquez <evazquez@systemscomm.net>
2023-11-28 13:42:38 -0800

    V1.1.0

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,9 @@ function changeBgColor(color){
     const sec = document.querySelector('.sec');
     sec.style.background = color;
 }
+function menuToggle(){
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navigation = document.querySelector('.navigation');
+    toggleMenu.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
