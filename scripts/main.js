c31ec6b4b5a3a3705189efd88cd1ba95a0f79eea
Eliam Vazquez <bosc790@gmail.com>
2023-11-27 23:10:38 -0800

    V1.0.0

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.pepsi').src = anything;
+}
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
